baa5b58639da15d5e303a707e81772b9975f04e9
dhruvbaliyan <dhruvbaliyan1001@gmail.com>
2023-12-11 15:12:27 +0530

    chore: change serial to string

---
+++ prisma/migrations/20231211094215_008/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "post_matric_scholarship" ALTER COLUMN "serial" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "pre_matric_scholarship" ALTER COLUMN "serial" SET DATA TYPE TEXT;
